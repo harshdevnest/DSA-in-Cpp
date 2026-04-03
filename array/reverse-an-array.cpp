8721690f8b2504ecac594c4c1f8137b130ea1657
Harsh Mahajan <aneliteharsh@gmail.com>
2026-04-04 04:11:13 +0530

    added reversing array method

---
+++ array/reverse-an-array.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int n){
+    int start = 0;
+    int end = n - 1;
+
+    while(start < end){
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int arr[] = {1,2,3,4,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    cout<<"Original: ";
+    printArray(arr,n);
+
+    reverseArray(arr,n);
+
+    cout<<"Reversed: ";
+    printArray(arr,n);
+
+    return 0;
+}
