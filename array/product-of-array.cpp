1b4f46e3dec5c09aa662c95fd0368f11b4558325
Harsh Mahajan <aneliteharsh@gmail.com>
2026-04-08 19:43:17 +0530

    added product of array expected itself solution

---
+++ array/product-of-array.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+ void productArray(int arr[],int size){
+    int temp[size];
+    for (int i = 0; i < size; i++)
+    {   int res = 1;
+        for (int j = 0; j < size; j++)
+        {
+            if (i == j) continue;
+
+            res = res * arr[j];
+        }
+        temp[i] = res;
+    }
+
+     for (int i = 0; i < size; i++)
+    {
+        cout<<temp[i]<<" ";
+    }
+    
+ }
+
+ int main(){
+    int arr[] = {2,6,3,4,5};
+    int len = sizeof(arr)/sizeof(arr[0]);
+
+    for (int i = 0; i < len; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+    productArray(arr,len);
+
+    return 0;
+ }
