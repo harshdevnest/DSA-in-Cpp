3f22be58eba8d49b7539080e304246b7d4f1d3be
Harsh Mahajan <aneliteharsh@gmail.com>
2026-05-01 00:59:42 +0530

    updated variable with pointer

---
+++ pointer/how-to-update-value-using-pointer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//value updation using function 
+int update(int* x){
+    *x = 30;
+    return *x;
+}
+int main (){
+    int a = 10 ;
+    cout<<"value of A :"<<a<<endl;
+
+    int* ptr= &a;
+    *ptr= 20;
+    cout<<"value of A after update:"<<a<<endl;
+
+    cout<<"value after updated by function:"<<update(&a)<<endl;
+} 
