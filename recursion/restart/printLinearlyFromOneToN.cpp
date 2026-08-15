00dca93789a161e917223764952640c6804725ef
Harsh Mahajan <aneliteharsh@gmail.com>
2026-08-16 03:01:46 +0530

    printied numbers linearly

---
+++ recursion/restart/printLinearlyFromOneToN.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printNumber(int n, int temp)
+{
+
+    if (temp > n)
+    {
+        return;
+    }
+    cout << temp << endl;
+    temp++;
+    printNumber(n, temp);
+}
+
+int main()
+{
+    int n ,temp=1;
+
+    cout << "enter N:" << endl;
+    cin >> n;
+    printNumber(n,temp);
+    return 0;
+}
