d839dc58a863a6de035c7c1d54c90be02899341a
Harsh Mahajan <aneliteharsh@gmail.com>
2026-08-21 04:57:02 +0530

    polished the code using function

--- hashing/detectTheFrequencyofElements.cpp
+++ hashing/detectTheFrequencyofElements.cpp
@@ -1,28 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//pre-positioning
+int hashFunction(int arr[], int queryNumber , int n){
+    int hash[13] = {0};       // here we ssumed that the array has max element = 12 
+
+    for (int i = 0; i < n; i++)
+    {
+        hash[arr[i]] +=1;
+    }
+
+    return hash[queryNumber];
+}
+
+// size of array
+// int findSizeofArray(int arr[]){
+//     int length = sizeof(arr)/sizeof(arr[0]);
+//     return length;
+// }
+
+
 int main() {
 
     int n;
+    cout<< "enter size of array:"<<endl;
     cin >> n;
+
     int arr[n];
+    cout<<"enter element of array:"<<endl;
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
     //precompute:
-    int hash[13] = {0};
-    for (int i = 0; i < n; i++) {
-        hash[arr[i]] += 1;
-    }
+    // int hash[13] = {0};
+    // for (int i = 0; i < n; i++) {
+    //     hash[arr[i]] += 1;
+    // }
 
     int q;
+    cout<<"enter number of queries :"<<endl;
     cin >> q;
+
+    cout<<"enter element to check there frequency:"<<endl;
+    
     while (q--) {
         int number;
         cin >> number;
         // fetching:
-        cout << hash[number] << endl;
+        cout <<"frequeny: "<< hashFunction(arr,number,n)<< endl;
     }
     return 0;
 }
