a18ef5e89df13a6d6e580773ff45b096024eb64b
Harsh Mahajan <aneliteharsh@gmail.com>
2026-09-01 03:06:10 +0530

    used insertion sort recursively

--- sorting/part-2/recursiveBubbleSort.cpp
+++ sorting/part-2/recursiveBubbleSort.cpp
@@ -2,15 +2,19 @@
 using namespace std;
 
 // Recursively sorts an array of size n
-void bubbleSort(vector<int>& arr, int n) {
+void bubbleSort(vector<int> &arr, int n)
+{
     // Base case: array of size 0 or 1 is already sorted
-    if (n <= 1) {
+    if (n <= 1)
+    {
         return;
     }
 
     // One pass: push the largest element to the end (index n - 1)
-    for (int i = 0; i < n - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
             swap(arr[i], arr[i + 1]);
         }
     }
@@ -19,23 +23,28 @@ void bubbleSort(vector<int>& arr, int n) {
     bubbleSort(arr, n - 1);
 }
 
-int main() {
+int main()
+{
+
     vector<int> arr = {3, 5, 1, 2, 4};
     int n = arr.size();
 
-    cout << " array before RecursBubble sort: "<<endl;
-    for (int i = 0; i < n; i++) {
+    cout << " array before RecursBubble sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
         cout << arr[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 
     bubbleSort(arr, n);
 
-    cout << " array after RecusBubble sort: "<<endl;
-    for (int i = 0; i < n; i++) {
+    cout << " array after RecusBubble sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
         cout << arr[i] << " ";
     }
     cout << "\n";
 
     return 0;
 }
+
