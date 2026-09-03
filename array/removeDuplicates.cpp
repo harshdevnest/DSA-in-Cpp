09a1fc66176e9aa64fa72652579dfe1bf13627a7
Harsh Mahajan <aneliteharsh@gmail.com>
2026-09-04 00:36:38 +0530

    practiced array basics

--- array/removeDuplicates.cpp
+++ array/removeDuplicates.cpp
@@ -1,43 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Class to hold the solution logic
-class Solution {
+class Solution
+{
 public:
-    // Function to remove duplicates from sorted array in-place
-    int removeDuplicates(vector<int>& nums) {
-        // If array is empty, return 0 directly
-        if (nums.empty()) return 0;
-
-        // Pointer for the position of last unique element
+    int removeDuplicates(int arr[], int n)
+    {
         int i = 0;
-
-        // Traverse the array starting from the second element
-        for (int j = 1; j < nums.size(); j++) {
-            // If current element is different from last unique element
-            if (nums[j] != nums[i]) {
-                // Move pointer for unique element forward
+        for (int j = 1; j < n; j++)
+        {
+            if (arr[i] != arr[j])
+            {
                 i++;
-                // Place the new unique element at the next position
-                nums[i] = nums[j];
+                arr[i] = arr[j];
             }
         }
-
-        // i is index of last unique element, count = i + 1
-        return i + 1;
+        return i+1;
     }
 };
 
-int main() {
-    vector<int> nums = {0,0,1,1,1,2,2,3,3,4};
-
+int main(){
+    int arr[6] = {1, 1, 3, 5, 6, 8};
     Solution sol;
-    int k = sol.removeDuplicates(nums);
 
-    cout << "Unique count = " << k << "\n";
-    cout << "Array after removing duplicates: ";
-    for (int x = 0; x < k; x++) {
-        cout << nums[x] << " ";
+    int k= sol.removeDuplicates(arr,6);
+    cout<<"array has "<<k<<" unique elments"<<endl;
+    cout<< "unique elements of array: "<<endl;
+    for (int i = 0; i < k; i++)
+    {
+        cout<<arr[i]<<" ";
     }
-    cout << endl;
+    
+    return 0;
 }
+// in  this appraoach to remove duplicates we have used a pointer which moves only when there is an unique element appears in the array 
